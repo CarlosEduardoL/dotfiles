bf1f836b8ba5bfa38082ca1a1714650a88091ef7
CarlosEduardoL <carlos.lizalda1@gmail.com>
2023-05-17 14:00:36 -0500

    Add some aliases

--- .zshrc
+++ .zshrc
@@ -1,6 +1,8 @@
 export HISTFILE=~/.zsh_history
 export HISTSIZE=100000
 
+[[ -f "$DOT_DIR/custom_config.zsh"]] && source "$DOT_DIR/custom_config.zsh"
+
 source "$DOT_DIR/aliases.zsh"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,13 @@
+# Tooling aliases
 alias vi='lvim' # You can change it to nvim or remove it if you want
-alias reload='exec zsh' # You can just source your .zshrc or .zshenv if you want
 alias ls='exa --icons'
 alias cat='bat'
+
+# ls aliases
+alias ll='ls -l'
+alias la='ll -a'
+
+
+# Utility aliases
+
+alias reload='exec zsh' # You can just source your .zshrc or .zshenv if you want
